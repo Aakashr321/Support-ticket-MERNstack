949f35edf0ab815526ef41ad1a6a893da1a90a8c
Aakashr321 <aakash.rathore321@gmail.com>
2023-04-10 09:15:20 +0530

    Backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 85000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support desk API" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port http://localhost:${PORT}`);
+});
